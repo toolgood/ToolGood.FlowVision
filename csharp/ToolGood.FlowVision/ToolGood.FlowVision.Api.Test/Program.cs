ad260584efff8c847947867486ed779e2fa1341c
linzhijun <at6180686@qq.com>
2022-12-07 12:54:53 +0800

    添加 README.md

---
+++ csharp/ToolGood.FlowVision/ToolGood.FlowVision.Api.Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolGood.FlowVision.Api.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PetaTest.Runner.RunMain(args);
+        }
+    }
+}
